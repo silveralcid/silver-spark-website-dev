ee3cb8c1ee02cf8cc354f4de3b51a706633d706b
Silver Alcid <silveralcid@gmail.com>
2025-04-05 11:51:16 -0400

    about us, fixed bold issue

--- src/components/RichText/index.tsx
+++ src/components/RichText/index.tsx
@@ -67,7 +67,7 @@ export default function RichText(props: Props) {
     <ConvertRichText
       converters={jsxConverters}
       className={cn(
-        'payload-richtext',
+        'payload-richtext rich-text-preserve-bold',
         {
           container: enableGutter,
           'max-w-none': !enableGutter,
